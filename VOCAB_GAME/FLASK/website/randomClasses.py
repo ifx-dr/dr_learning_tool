909b2ef02a3f5550fb8a7149a5bc153c2381584b
Rituparna Bose <rituparna.bose@infineon.com>
2026-05-17 19:13:04 +0200

    Added classes from Sustainability_Lobe

--- VOCAB_GAME/FLASK/website/randomClasses.py
+++ VOCAB_GAME/FLASK/website/randomClasses.py
@@ -686,8 +686,17 @@ random_words_map = {
     686: "http://www.w3id.org/ecsel-dr-AT#IEC_Standard",
     687: "http://www.w3id.org/ecsel-dr-AT#IEEE_Standard",
     688: "http://www.w3id.org/ecsel-dr-AT#ISA_95_Layer_4_ERP",
-    689: "http://www.w3id.org/ecsel-dr-AT#OCF_Standard"
+    689: "http://www.w3id.org/ecsel-dr-AT#OCF_Standard",
+    690: "http://www.w3id.org/ecsel-dr-CO2Savings#CO2_Burden_Of_Chemicals_In_The_Front_End_Manufacturing",
+    691: "http://www.w3id.org/ecsel-dr-CO2Savings#Average_Lifetime_Of_A_Wind_Turbine",
+    692: "http://www.w3id.org/ecsel-dr-CO2Savings#Average_Lifetime_Solar_Panel",
+    693: "http://www.w3id.org/ecsel-dr-CO2Savings#Average_Usage_Time_Per_Year_Solar_Panel",
+    694: "http://www.w3id.org/ecsel-dr-CO2Savings#Back_End_Manufacturing",
+    695: "http://www.w3id.org/ecsel-dr-BMS#Battery_CO2_Savings",
+    696: "http://www.w3id.org/ecsel-dr-DF#Bill_Of_Material_(BOM)",
+    697: "http://www.w3id.org/ecsel-dr-SO#Bottleneck",
+    698: "http://www.w3id.org/ecsel-dr-CO2Savings#CO2_Burden_Of_Gases_In_The_Front_End_Manufacturing",
+    699: "http://www.w3id.org/ecsel-dr-BMS#Cells"
   
     
-
 }
